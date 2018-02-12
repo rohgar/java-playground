ae6435276f3f1a799a2099085996261607c4c6a7
garroh <garroh99@gmail.com>
2018-02-11 19:09:11 -0800

    added a java reference test

---
+++ src/com/test/DefaultValues.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class DefaultValues
+{
+
+	private static int intVal;
+	private static long longVal;
+	private static float floatVal;
+	private static double doubleVal;
+	private static char charVal;
+	private static boolean booleanVal;
+	private static String StringVal;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Default int = " + intVal);
+		System.out.println("Default long = " + longVal);
+		System.out.println("Default float = " + floatVal);
+		System.out.println("Default double = " + doubleVal);
+		System.out.println("Default char = " + charVal);
+		System.out.println("Default boolean = " + booleanVal);
+		System.out.println("Default String = " + StringVal);
+	}
+	
+}
---
+++ src/com/test/Reference.java
@@ -0,0 +1,44 @@
+package com.test;
+
+public class Reference
+{
+
+	private static class Node {
+	    public Integer data = null;
+	    public Node next = null; 
+
+	    public Node(int data) {
+	        this.data = data;
+	    }
+	    
+	    @Override
+	    public String toString()
+	    {	
+	    		StringBuffer sb = new StringBuffer();
+	    		Node temp = this;
+	    		while (temp != null)
+	    		{
+	    			sb.append(temp.data + ", ");
+	    			temp = temp.next;
+	    		}
+	    		return sb.toString();
+	    }
+	}
+	
+	public static void main( String[] args)
+	{
+		Node n1 = new Node(1);
+	    Node n2 = new Node(2);
+	    Node n3 = new Node(3);
+	    Node n4 = new Node(4);
+	    n1.next = n2;
+	    n2.next = n3;
+	    n3.next = n4;
+	    
+	    System.out.println(n1);
+	    System.out.println(n2);
+	    n2 = n3;
+	    System.out.println(n1);
+	    System.out.println(n2);
+	}
+}
