5d5255a6055301a2ad092d2263a8d3f84dd45fb9
garroh <garroh99@gmail.com>
2018-03-31 18:58:24 -0700

    recursion

--- src/com/test/binary/BitRepresentation.java
+++ src/com/test/binary/BitRepresentation.java
@@ -8,7 +8,5 @@ public class BitRepresentation
 		System.out.println("num = " + Integer.toBinaryString(1 << 0)); // prints 1
 		System.out.println("num = " + Integer.toBinaryString(1 << 1)); // prints 10
 		System.out.println("num = " + Integer.toBinaryString(-1 << 1)); // prints 11111111111111111111111111111110
-		
-		
 	}
 }
---
+++ src/com/test/recursion/Recursion01.java
@@ -0,0 +1,28 @@
+package com.test.recursion;
+
+public class Recursion01
+{
+	
+	public static void main(String[] args)
+	{
+		int n = 2;
+		test(n, n);
+	}
+	
+	private static void test(int nLeft, int nRight)
+	{
+		if (nLeft < 0 || nRight < 0)
+		{
+			return;
+		}
+		System.out.println(String.format("(%d, %d)", nLeft, nRight));
+		// System.out.println(String.format("nLeft = %d, nRight = %d", nLeft, nRight));
+		if (nLeft == 0 && nRight == 0)
+		{
+			System.out.println("-----------------------");
+		}
+		test(nLeft - 1, nRight);
+		test(nLeft, nRight - 1);
+	}
+	
+}
